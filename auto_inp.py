01198b4f4e339114127383c19daf1f6487a0e945
earthscience2 <slhg@hanmail.net>
2025-06-13 18:06:14 +0900

    inp test 16

--- auto_inp.py
+++ auto_inp.py
@@ -66,21 +66,22 @@ def make_inp(concrete, sensor_data_list, latest_csv):
     start_time = latest_csv
     time_list = get_hourly_time_list(start_time)
     sensor_count = len(sensor_data_list)
-    for time in time_list:
-        sensors = []
+    if sensor_count != 0:
+        for time in time_list:
+            sensors = []
 
-        for sensor in sensor_data_list:
-            sensor_data_df = api_db.get_sensor_data_by_time(sensor_pk=sensor['sensor_pk'], time=time)
-            position = json.loads(sensor['dims'])['nodes']
-            if not sensor_data_df.empty:
-                row_dict = sensor_data_df.iloc[0].to_dict()
-                temp = row_dict['temperature']
-                sensors.append((0, position[0], position[1], position[2], float(temp)))
-            else:
-                temp = None
-                
-        if sensor_count == len(sensors):
-            print(plan_points, thickness, element_size, time, sensors)
+            for sensor in sensor_data_list:
+                sensor_data_df = api_db.get_sensor_data_by_time(sensor_pk=sensor['sensor_pk'], time=time)
+                position = json.loads(sensor['dims'])['nodes']
+                if not sensor_data_df.empty:
+                    row_dict = sensor_data_df.iloc[0].to_dict()
+                    temp = row_dict['temperature']
+                    sensors.append((0, position[0], position[1], position[2], float(temp)))
+                else:
+                    temp = None
+
+            if sensor_count == len(sensors):
+                print(plan_points, thickness, element_size, time, sensors)
 
 
 # 센서 데이터 자동 저장 및 업데이트
