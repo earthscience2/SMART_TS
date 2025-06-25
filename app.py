2c80231e4b53e4b430c0ca4a306c5abfbb7f9847
earthscience2 <slhg@hanmail.net>
2025-06-25 18:04:46 +0900

    홈페이지 수정 4

--- app.py
+++ app.py
@@ -197,4 +197,4 @@ def update_nav_active(pathname):
     return classes
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=23022)
