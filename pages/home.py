3a1deb7a389c88c519a071ec64aa47cb49ef8c87
earthscience2 <slhg@hanmail.net>
2025-07-06 01:36:53 +0900

    홈페이지 26

--- pages/home.py
+++ pages/home.py
@@ -242,7 +242,7 @@ def layout():
                                             style={
                                                 "textDecoration": "none",
                                                 "borderRadius": "6px",
-                                                "padding": "8px 24px",
+                                                "padding": "8px 18px",
                                                 "height": "40px",
                                                 "display": "flex",
                                                 "alignItems": "center",
@@ -263,7 +263,7 @@ def layout():
                                             style={
                                                 "textDecoration": "none",
                                                 "borderRadius": "6px",
-                                                "padding": "8px 32px",
+                                                "padding": "8px 18px",
                                                 "height": "40px",
                                                 "display": "flex",
                                                 "alignItems": "center",
@@ -284,7 +284,7 @@ def layout():
                                             style={
                                                 "textDecoration": "none",
                                                 "borderRadius": "6px",
-                                                "padding": "8px 24px",
+                                                "padding": "8px 18px",
                                                 "height": "40px",
                                                 "display": "flex",
                                                 "alignItems": "center",
@@ -305,7 +305,7 @@ def layout():
                                             style={
                                                 "textDecoration": "none",
                                                 "borderRadius": "6px",
-                                                "padding": "8px 32px",
+                                                "padding": "8px 18px",
                                                 "height": "40px",
                                                 "display": "flex",
                                                 "alignItems": "center",
